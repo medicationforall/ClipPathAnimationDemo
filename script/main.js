dac8027ae7a1d023ed087ea0be245b3343db472e
ADAMS James <james@medicationforall.com>
2018-01-27 06:56:29 -0500

    The Code.

---
+++ script/main.js
@@ -0,0 +1,31 @@
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$(document).ready(function(){
+  console.log('document ready');
+  
+  $('.button').on('click',function(event){
+    event.preventDefault();
+    var shape= $(this).data('shape');
+    console.log('clicked shape',shape);
+    
+    var fromShapes = ['square','rabet','triangle','pentagon'];
+    
+    for(var i=0,shape;(fromShape=fromShapes[i]);i++){
+      if(fromShape !== shape && shape !== 'circle' && $('.shape').hasClass(fromShape)){
+        var animationClass = fromShape+'-to-'+shape;
+        $('.shape').animateCss(animationClass);
+        break;
+      }  
+    }
+     
+    
+    $('.shape').removeClass('square circle triangle pentagon rabet').addClass(shape);
+  });
+});
